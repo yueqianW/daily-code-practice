fa869cff021258986dbe29d2bd5ceb514be9fd41
yueqianW <ames.yueqian@hotmail.com>
2019-06-02 10:46:27 +0800

    add loader

--- echarts/webpack.config.js
+++ echarts/webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path')
 
 module.exports = {
-    entry: "./index.js",
+    entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: "bundle.js"
     },
     module: {
 
-    }
+    },
+    devServer: {
+        contentBase: './dist',
+        watchContentBase: true
+    },
 };
