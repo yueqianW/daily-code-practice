a7555e56b482aa90ade9f048d5bde5d149ba8ecd
yueqianW <ames.yueqian@hotmail.com>
2019-06-11 14:59:00 +0800

    add for loop test

---
+++ javascript/es6/let/let.js
@@ -0,0 +1,13 @@
+var print
+
+for(var i=0;i<3;i++){
+	if(i==2){
+		print = function (){
+			// return i;
+			console.log(i)
+		}
+	}
+}
+
+// console.log(print())
+print()
---
+++ javascript/js advanced/for.js
@@ -0,0 +1,14 @@
+var test = [
+    {
+        a:1,
+        b:2
+    },
+    {
+        a:3,
+        b:4
+    }
+]
+
+for (let key of test){
+    console.log(key)
+}
