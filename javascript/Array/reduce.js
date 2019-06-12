ca6f588f0b88b661a8f87e8019c0c3fe7fca5f5b
yueqianW <ames.yueqian@hotmail.com>
2019-06-12 15:18:32 +0800

    update array & console

--- javascript/Array/reduce.js
+++ javascript/Array/reduce.js
@@ -1,20 +1,18 @@
 var value = []
 var n = 0;
 
-for(i=1;i<1000001;i++){
+for (i = 1; i < 10001; i++) {
     value.push(i)
 }
 
-console.time('reduce')
+// reduce
 var sum = value.reduce((prev, cur, index, array) => {
     return prev + cur;
 })
-console.timeEnd('reduce')
 
-console.time('for')
+// for
 for (let i = 0; i < value.length; i++) {
     n += value[i]
 }
-console.timeEnd('for')
 
 console.log('-------------', sum, n)
