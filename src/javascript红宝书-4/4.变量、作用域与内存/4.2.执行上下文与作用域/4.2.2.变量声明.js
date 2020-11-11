76ab0424e9b654ea0c2eba1c285cda48ec028a33
yueqianW <ames.yueqian@hotmail.com>
2020-11-11 13:02:09 +0800

    add: 变量

---
+++ src/javascript红宝书-4/4.变量、作用域与内存/4.2.执行上下文与作用域/4.2.2.变量声明.js
@@ -0,0 +1,16 @@
+function add(num1, num2) {
+  sum = num1 + num2;
+  return sum
+}
+
+let result = add(10, 10)
+
+console.log(result, sum)
+
+
+if(sum > 10) {
+
+  let test = 5
+  console.log(test);
+
+}
