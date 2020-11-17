023fe65527e8c7e5586061e73e59a4206c709552
yueqianW <ames.yueqian@hotmail.com>
2020-11-17 17:54:13 +0800

    add: flat

--- src/javascript/for.js
+++ src/javascript/for.js
@@ -1,12 +1,12 @@
 let a = [1, 2, 3]
 
 for (var i = 1; i < a.length; i++) {
-  console.log(a[i])
+  // console.log(a[i])
 }
 
-console.log(i)
+// console.log(i)
 
 let b = `a
 2`
 
-console.log(b)
+// console.log(b)
